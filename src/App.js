7064d96b956df071c56fade0dada6d6410075c7c
rimmygit <gpfla5503@gmail.com>
2023-10-21 17:27:48 +0900

    react useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time"); //state가 변경될때마다 실행됨
+
+  // 2.
+  // 간략화 한 버전
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
+
+  // 1.
+  // const iRunOnlyOne = () => {
+  //   // 하지만 이 코드는 한 번만 실행됨 useEffect 때문
+  //   console.log("i run only once");
+  // };
+  // useEffect(iRunOnlyOne, []);
   return (
     <div>
-      <h1 className={styles.title}>welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
