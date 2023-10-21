ec031262654ea06f654d3a230d304cce2141d93c
rimmygit <gpfla5503@gmail.com>
2023-10-21 23:07:49 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,45 +1,35 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  // 여러개의 to를 가질 수 있는 array생성, 기본값 비워져있음
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value); // 값 수정하기
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
-  const onSubmit = (event) => {
-    event.preventDefault(); // 자동으로 새로고침 하는걸 막음
-    if (toDo === "") {
-      // 만약에 todo가 비어있으면 그냥 리턴, 함수를 작동시키지 않게끔
-      return;
-    }
-    // 빈 array에 submit이벤트 실행 시 값이 추가됨
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    // input 비우기
-    setToDo("");
-  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      // json 코인을 얻었을때 json의 값을 setCoins해줘, setLoading값은 false로 바꿔줘
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
-  // form은 submit 이벤트를 가지고 있어서 자동으로 새로고침됨
-  // form 안의 버튼들을 모두 실행
+  // map을 사용하면 react.js는 element에 key를 주도록 함
+  // 여기서는 api가 id를 가지고 있으니 이걸 key로 쓰면 됨
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading ...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
