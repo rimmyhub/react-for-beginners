1b3abf5e2160a2ed1bff3a0b8c996ee0d7b7e39a
rimmygit <gpfla5503@gmail.com>
2023-10-22 12:57:57 +0900

    movie json response

--- src/App.js
+++ src/App.js
@@ -2,36 +2,43 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      // json 코인을 얻었을때 json의 값을 setCoins해줘, setLoading값은 false로 바꿔줘
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-
-  // map을 사용하면 react.js는 element에 key를 주도록 함
-  // 여기서는 api가 id를 가지고 있으니 이걸 key로 쓰면 됨
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading ...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2> {movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
     </div>
   );
 }
-
 export default App;
